6ea7ff6374c6b6ad36ab21040a317685e29c7a84
Chris Pyles <cpyles@berkeley.edu>
2019-06-25 19:34:33 -0700

    file pruning

---
+++ demo/tests.py
@@ -0,0 +1,15 @@
+answers = [
+	{
+		"identifier": "q1",
+		"answer": 3,
+		"points": 1,
+	}, {
+		"identifier": "q2",
+		"answer": 2,
+		"points": 2,
+	}, {
+		"identifier": "q3",
+		"answer": "D",
+		"points": 3,
+	}
+]
