c86078c7f67afd2fdf99c43c49178fa93d554f27
Chris Pyles <cpyles@berkeley.edu>
2019-08-20 11:40:54 -0700

    changed .gitmodules

--- mcautograder/notebook.py
+++ mcautograder/notebook.py
@@ -2,71 +2,120 @@
 ##### Multiple Choice Question Autograder #####
 ###############################################
 
+import os
 import string
 import re
 import runpy
-from .utils import *
+from utils import *
 
 class Notebook:
 	"""Multiple choice question autograder for Jupyter Notebook"""
 
-	def __init__(self, tests, scored=False, max_attempts=None):
+	def __init__(self, tests=".tests.py", scored=False, max_attempts=None):
 		"""
 		Initlaizes multiple choice autograder.
     
 		Args:
-			tests (``str``): The relative filepath to tests file
+			tests (`str`): The relative filepath to tests file
 			
 		Kwargs:
-			scored (``bool``): Whether or not the assignment is scored
-			max_attempts (``int``): The maximum number of takes allowed; deault ``None``
+			scored (`bool`): Whether or not the assignment is scored
+			max_attempts (`int`): The maximum number of takes allowed; deault `None`
 
 		Returns:
-			``Notebook``. The ``Notebook`` instance for the autograder
+			`Notebook`. The `Notebook` instance for the autograder
 		"""
-		self._tests_raw = runpy.run_path(tests)["answers"]
-		self._identifiers = [answer["identifier"] for answer in self._tests_raw]
-		self._tests = {identifier : test for identifier, test in zip(
-			self._identifiers,
-			self._tests_raw
-		)}
+		if os.path.exists(".MCAUTOGRADER_STATUS") and os.path.isfile(".MCAUTOGRADER_STATUS"):
+			with open(".MCAUTOGRADER_STATUS", "rb") as f:
+				self.__dict__ = pickle.load(f).__dict__
 
-
-
-		self._scored = scored
-		if self._scored:
-			try:
-				self._points = {identifier : self._tests[identifier]["points"] for identifier in self._identifiers}
-			except KeyError:
-				assert False, "One or more identifiers missing points value in scored notebook"
-			self._answered = {identifier : false for identifier, false in zip(
-				self._identifiers, 
-				repeat(False, len(self._identifiers))
+		else:
+			self._tests_raw = runpy.run_path(tests)["answers"]
+			self._identifiers = [answer["identifier"] for answer in self._tests_raw]
+			self._tests = {identifier : test for identifier, test in zip(
+				self._identifiers,
+				self._tests_raw
 			)}
-			self._possible = sum(self._points.values())
-			assert self._possible > 0, "Scored notebooks must have > 0 points possible"
-			self._earned = 0
 
-		self._inf_retakes = True
-		if max_attempts:
-			assert max_attempts > 0 and type(max_attempts) == int, "max_attempts must be a positive integer"
-			self._inf_retakes = False
-			self._retakes = {identifier : zero for identifier, zero in zip(
-				self._identifiers, 
-				repeat(0, len(self._identifiers))
-			)}
-			self._max_attempts = max_attempts
+			self._scored = scored
+			if self._scored:
+				try:
+					self._points = {identifier : self._tests[identifier]["points"] for identifier in self._identifiers}
+				except KeyError:
+					assert False, "One or more identifiers missing points value in scored notebook"
+				self._answered = {identifier : false for identifier, false in zip(
+					self._identifiers, 
+					repeat(False, len(self._identifiers))
+				)}
+				self._possible = sum(self._points.values())
+				assert self._possible > 0, "Scored notebooks must have > 0 points possible"
+				self._earned = 0
+
+			self._inf_retakes = True
+			if max_attempts:
+				assert max_attempts > 0 and type(max_attempts) == int, "max_attempts must be a positive integer"
+				self._inf_retakes = False
+				self._retakes = {identifier : zero for identifier, zero in zip(
+					self._identifiers, 
+					repeat(0, len(self._identifiers))
+				)}
+				self._max_attempts = max_attempts
+
+			if self._scored and not self._inf_retakes:
+				self.__dict__ = dict(
+					_identifiers = self._identifiers,
+					_tests = self._tests,
+					_scored = self._scored,
+					_inf_retakes = self._inf_retakes,
+					_points = self._points,
+					_answered = self._answered,
+					_possible = self._possible,
+					_earned = self._earned,
+					_retakes = self._retakes,
+					_max_attempts = self._max_attempts
+				)
+			elif self._scored:
+				self.__dict__ = dict(
+					_identifiers = self._identifiers,
+					_tests = self._tests,
+					_scored = self._scored,
+					_inf_retakes = self._inf_retakes,
+					_points = self._points,
+					_answered = self._answered,
+					_possible = self._possible,
+					_earned = self._earned
+				)
+			elif not self._inf_retakes:
+				self.__dict__ = dict(
+					_identifiers = self._identifiers,
+					_tests = self._tests,
+					_scored = self._scored,
+					_inf_retakes = self._inf_retakes,
+					_retakes = self._retakes,
+					_max_attempts = self._max_attempts
+				)
+			else:
+				self.__dict__ = dict(
+					_identifiers = self._identifiers,
+					_tests = self._tests,
+					_scored = self._scored,
+					_inf_retakes = self._inf_retakes
+				)
+
+	def _serialize(self):
+		with open(".MCAUTOGRADER_STATUS", "wb+") as f:
+			serialize(self, f)
 
 	def _check_answer(self, identifier, answer):
 		"""
 		Checks whether or not answer is correct; returns boolean
     
 		Args:
-			identifier (``str``): The question identifier
-			answer (``str``, ``int``): The student's answer
+			identifier (`str`): The question identifier
+			answer (`str`, `int`): The student's answer
 
 		Returns:
-			``bool``. Whether or not the answer is correct
+			`bool`. Whether or not the answer is correct
 		"""
 		assert identifier in self._identifiers, "{} is not in the question bank".format(identifier)
 		assert type(answer) in [str, int], "Answer must be a string or integer"
@@ -96,15 +145,15 @@ class Notebook:
 
 	def check(self, identifier, answer):
 		"""
-		Visible wrapper for ``Notebook._check_answer`` to print output based on whether or not student's
+		Visible wrapper for `Notebook._check_answer` to print output based on whether or not student's
 		answer is correct
 
     	Args:
-			identifier (``str``): The question identifier
-			answer (``str``, ``int``): The student's answer
+			identifier (`str`): The question identifier
+			answer (`str`, `int`): The student's answer
 
 		Returns:
-			``None``. Prints out student's result on question
+			`None`. Prints out student's result on question
 		"""
 		result = self._check_answer(identifier, answer)
 		if self._scored:
@@ -122,6 +171,8 @@ class Notebook:
 			else:
 				print("Try again.")
 
+		self._serialize()
+
 	def score(self):
 		"""
 		If assignment is scored, displays student's score as fraction and percentage.
@@ -130,3 +181,5 @@ class Notebook:
 			print("{}/{}: {:.3f}%".format(self._earned, self._possible, self._earned/self._possible*100))
 		else:
 			print("This notebook is not scored.")
+
+		self._serialize()
--- mcautograder/utils.py
+++ mcautograder/utils.py
@@ -2,15 +2,23 @@
 ##### Utilities for mcautograder library #####
 ##############################################
 
+import pickle
+
 def repeat(x, n):
 	"""
 	Returns a list of a given value repeated a given number of times
 
 	Args:
 		x (any): The value to repeat
-		n (``int``): The number of repetitions
+		n (`int`): The number of repetitions
 
 	Returns:
-		``list``. List of repeated values ``x``
+		`list`. List of repeated values `x`
 	"""
 	return [x for _ in range(n)]
+
+def serialize(obj, file):
+	"""
+	Serializes OBJ and writes contents to FILE
+	"""
+	pickle.dump(obj, file, protocol=pickle.HIGHEST_PROTOCOL)
